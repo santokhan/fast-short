1c3e6ed7ca4ec33fbf4599de3beebe3503983974
santokhan <santokhan1999@gmail.com>
2024-04-28 00:19:55 +0600

    add adsense to layout

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/navbar/NavBar";
 import AuthProvider from "@/context/AuthContext";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4863734906175420"
+          crossOrigin="anonymous"
+        />
         <AuthProvider>
           <NavBar />
           {children}
