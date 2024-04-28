c3a5cf1a70a9201de4332b9136121f55b884b6fc
santokhan <santokhan1999@gmail.com>
2024-04-28 18:51:33 +0600

    update client form also

--- components/form/URLShorter.jsx
+++ components/form/URLShorter.jsx
@@ -1,132 +1,84 @@
 'use client'
 
-import checkAlias from '@/actions/checkAlias'
+import checkAlias from '@/actions/query/checkAlias'
+import createURL from '@/actions/query/createURL'
 import storeURLAlias from '@/actions/storeURLAlias'
 import storeURLHash from '@/actions/storeURLHash'
 import shortenUrlToId from '@/lib/shorter/shortid'
 import React, { useState } from 'react'
 import placeholder from './shorter-form/placeholder'
-import domains from './shorter-form/domains'
-import { redirect } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 import { storeToLocal } from '@/lib/local-storage/localStorage'
 import SubmitButton from './shorter-form/SubmitButton'
 import { twMerge } from 'tailwind-merge'
-
-const DomainSelect = ({ value, onChange }) => {
-    return (
-        <select
-            value={value}
-            onChange={(e) => onChange(e.target.value)}
-            className='block px-4 py-4 w-full rounded-lg bg-gray-50'
-            placeholder='Domain'
-        >
-            {domains.map((domain) => (
-                <option key={domain.value} value={domain.value}>
-                    {domain.value}
-                </option>
-            ))}
-        </select>
-    )
-}
+import { onSubmitShortForm } from '@/actions/onSubmit'
+import DomainSelect from './shorter-form/SelectDomain'
+import initialURL from '@/utils/initialURL'
+import FormTitle from '@/components/form/FormTitle'
 
 const UrlShorterForm = () => {
-    const [url, setUrl] = useState('')
+    const [url, setUrl] = useState(initialURL)
+    // const [url, setUrl] = useState('')
     const [domain, setDomain] = useState('fastshort.xyz')
     const [alias, setAlias] = useState('')
     const [error, setError] = useState({})
-
     const [loading, setLoading] = useState(false)
+    const router = useRouter()
 
-    function createWithAlias(url, domain, alias) {
-        // Check existance of alias
-        if (!url || !domain || !alias) {
-            console.error('Invalid parameters provided to createWithAlias')
-            return
-        } else {
-            setLoading(true)
+    const handleSubmit = async (e) => {
+        e.preventDefault()
 
-            checkAlias(domain, alias)
-                .then((result) => {
-                    if (result) {
+        if (url && domain) {
+            if (alias) {
+                setLoading(true)
+
+                try {
+                    const isExists = await checkAlias(domain, alias)
+
+                    if (isExists) {
                         setError((prev) => {
                             setLoading(false)
                             return { ...prev, alias: 'Alias already exists' }
                         })
                     } else {
-                        // Create new Docuemnt if the Alas not exists
-                        storeURLAlias(url, domain, alias)
-                            .then((res) => {
-                                setLoading(false)
+                        const res = await createURL(url, domain, '', alias, '')
 
-                                setUrl('')
-                                // setDomain('fastshort.xyz')
-                                setAlias('')
+                        if (res) {
+                            setLoading(false)
 
-                                storeToLocal(res)
+                            setUrl('')
+                            setAlias('')
 
-                                redirect('/generating/' + alias)
-                            })
-                            .catch((err) => {
-                                console.error(err)
-                            })
+                            storeToLocal(res)
+
+                            router.push(`/generating/${alias}`)
+                        }
                     }
-                })
-                .catch((err) => {
-                    console.error(err)
-                })
-        }
+                } catch (error) {
+                    console.error(error)
+                }
+            } else {
+                setLoading(true)
 
-        console.log(url, alias)
-    }
+                // Shorten the URL to a unique ID
+                try {
+                    const shortenedId = shortenUrlToId(url)
 
-    function createWithUniqueId(url, domain) {
-        // Check for null or undefined parameters
-        if (!url || !domain) {
-            console.error('Invalid parameters provided to createWithUniqueId')
-            return
-        } else {
-            setLoading(true)
+                    const res = await storeURLHash(url, domain, shortenedId)
 
-            // Shorten the URL to a unique ID
-            try {
-                const shortenedId = shortenUrlToId(url)
-
-                // Construct the indentifier
-                const indentifier = domain + '/' + shortenedId
-
-                // Do something with the form data (e.g. submit to server)
-                console.log(url, indentifier)
-
-                storeURLHash(url, domain, shortenedId)
-                    .then((res) => {
+                    if (res) {
                         setLoading(false)
 
                         setUrl('')
-                        // setDomain('fastshort.xyz')
                         setAlias('')
 
                         storeToLocal(res)
 
-                        redirect('/generating/' + alias)
-                    })
-                    .catch((err) => {
-                        console.error(err)
-                    })
-            } catch (err) {
-                console.error(`Error in createWithUniqueId: ${err.message}`)
-                return
-            }
-        }
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-
-        if (url && domain) {
-            if (alias) {
-                createWithAlias(url, domain, alias)
-            } else {
-                createWithUniqueId(url, domain)
+                        router.push(`/generating/${shortenedId}`)
+                    }
+                } catch (err) {
+                    console.error(err)
+                }
             }
         }
     }
@@ -142,11 +94,12 @@ const UrlShorterForm = () => {
 
     return (
         <div className='p-6 lg:p-8 rounded-xl bg-white space-y-6 mx-auto max-w-2xl'>
-            <h1 className='text-3xl font-bold text-center'>Shorten a long URL</h1>
+            <FormTitle>Shorten a long URL</FormTitle>
             <form onSubmit={handleSubmit} className='flex flex-wrap gap-6'>
                 <label className='w-full flex flex-col items-start'>
                     <input
                         type='url'
+                        name='url'
                         value={url}
                         onChange={(e) => {
                             setUrl(e.target.value)
@@ -165,11 +118,13 @@ const UrlShorterForm = () => {
                             setError({})
                         }}
                         className={twMerge(inputStyle)}
+                        required={true}
                     />
                 </label>
                 <label className='flex-1'>
                     <input
                         type='text'
+                        name='alias'
                         value={alias}
                         onChange={(e) => {
                             setAlias(e.target.value)
