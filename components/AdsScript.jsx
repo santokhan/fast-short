41e77ca33a7077156fd5c6bf61c0ed418bd9b0cc
santokhan <santokhan1999@gmail.com>
2024-04-28 18:35:18 +0600

    create google app script component

---
+++ components/AdsScript.jsx
@@ -0,0 +1,11 @@
+import Script from 'next/script'
+
+const AdsScript = () => (
+    <Script
+        async
+        src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4863734906175420'
+        crossOrigin='anonymous'
+    />
+)
+
+export default AdsScript
