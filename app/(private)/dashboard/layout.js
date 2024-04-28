57a81651a6bdba87457851d8174aaccc28fa7527
santokhan <santokhan1999@gmail.com>
2024-04-28 18:37:12 +0600

    create private layout

---
+++ app/(private)/dashboard/layout.js
@@ -0,0 +1,10 @@
+const DashboardLayout = ({ children }) => {
+    return (
+        <main className="min-h-screen">
+            <div className="h-16"></div>
+            {children}
+        </main >
+    )
+};
+
+export default DashboardLayout
---
+++ app/(private)/layout.js
@@ -0,0 +1,10 @@
+import "./globals.css";
+import AuthProvider from "@/context/AuthContext";
+
+export default function RootLayoutPrivate({ children }) {
+  return (
+    <AuthProvider>
+      {children}
+    </AuthProvider>
+  );
+}
