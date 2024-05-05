041950acb866590d145a9b21153c26f6ec0755a7
santokhan <santokhan1999@gmail.com>
2024-05-05 11:09:04 +0600

    update script

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/navbar/NavBar";
-import Head from "next/head";
-import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,15 +12,14 @@ export const metadata = {
 export default async function RootLayout({ children }) {
   return (
     <html lang="en">
-      <script
-        id='ad-script-cdn'
-        async={true}
-        src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4863734906175420'
-        crossOrigin='anonymous'
-      />
-      <Head>
-        <meta name="google-adsense-account" content="ca-pub-4863734906175420" />
-      </Head>
+      <head>
+        <script
+          id='ad-script-cdn'
+          async={true}
+          src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4863734906175420'
+          crossOrigin='anonymous'
+        />
+      </head>
       <body className={inter.className}>
         {/* <Script
           id='ad-script-cdn'
