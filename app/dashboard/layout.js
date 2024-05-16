a553b564819b094cb9bf8a362e05b0f55eb94fbb
santokhan <santokhan1999@gmail.com>
2024-05-16 17:05:00 +0600

    update protected routes

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,20 +1,40 @@
+'use client';
+
 import SideNavs from "@/blocks/dashboard/sidebar/SideNavs";
 import NavBar from "@/components/navbar/NavBar";
 import AuthProvider from "@/context/AuthContext";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 
-export default function RootLayoutPrivate({ children }) {
-  return (
-    <AuthProvider>
-      <NavBar fluid={true} />
-      <main className={twMerge("min-h-screen pt-16", 'flex')}>
+const PrivatePages = ({ children }) => {
+  const { status, data, update, error } = useSession()
+
+  if (status === 'loading') {
+    return 'Authenticating'
+  } else if (status === 'authenticated') {
+    return (
+      <>
         <aside className="hidden lg:block basis-64 bg-white rounded-xl">
           <SideNavs />
         </aside>
         <div className="p-4 lg:p-8 basis-96 flex-grow">
           {children}
         </div>
-      </main >
+      </>
+    )
+  } else {
+    redirect('/auth/signin')
+  }
+}
+
+export default function Layout({ children }) {
+  return (
+    <AuthProvider>
+      <NavBar fluid={true} />
+      <main className={twMerge("min-h-screen pt-16", 'flex')}>
+        <PrivatePages>{children}</PrivatePages>
+      </main>
     </AuthProvider>
   );
 }
