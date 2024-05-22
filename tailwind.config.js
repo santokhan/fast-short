a1e6ad163238a4d55c2ff6e4c43d2c5b897a6c1a
santokhan <santokhan1999@gmail.com>
2024-05-22 15:57:24 +0600

    update

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        button: "rgb(247,153,109)",
+      },
+    },
+  },
+  plugins: [],
+}
+
