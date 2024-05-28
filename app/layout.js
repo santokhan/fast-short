0def53178a66cf6553f0e66657edc98eafcf0aba
itzshakeeb <itzshakeeb@gmail.com>
2024-05-28 22:09:07 +0600

    update seript

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -12,7 +13,23 @@ export default async function RootLayout({ children }) {
   return (
     <html lang='en'>
       <head>
-
+        <Script
+          id="ad-script"
+          async
+          type="text/javascript">
+          {`
+        atOptions = {
+          'key' : 'd592f8f84f38de0159699b8ddb6fc839',
+          'format' : 'iframe',
+          'height' : 250,
+          'width' : 300,
+          'params' : {}
+        };
+      `}
+        </Script>
+        <script
+          async
+          type="text/javascript" src="//www.topcreativeformat.com/d592f8f84f38de0159699b8ddb6fc839/invoke.js"></script>
         {/* <script
               id='ad-script-cdn'
               async={true}
@@ -21,6 +38,8 @@ export default async function RootLayout({ children }) {
             /> */}
       </head>
       <body className={inter.className}>
+        <div className='ad-script'>
+        </div>
         {children}
 
       </body>
--- app/page.js
+++ app/page.js
@@ -7,7 +7,6 @@ import Footer from '@/components/footer/Footer'
 import URLShort from '@/components/form/URLShorter'
 import NavBar from '@/components/navbar/NavBar'
 import AuthProvider from '@/context/AuthContext'
-import Script from 'next/script'
 
 export default async function URLShortFormPage({ searchParams }) {
   // const error = { ...searchParams }
@@ -23,23 +22,6 @@ export default async function URLShortFormPage({ searchParams }) {
             <URLShort />
           </AuthProvider>
         </Section>
-        <Script
-          id="ad-script"
-          async
-          type="text/javascript">
-          {`
-        atOptions = {
-          'key' : 'e937ad8f007592b5a24d9f9b987c1553',
-          'format' : 'iframe',
-          'height' : 60,
-          'width' : 468,
-          'params' : {}
-        };
-      `}
-        </Script>
-        <script
-          async
-          type="text/javascript" src="//www.topcreativeformat.com/e937ad8f007592b5a24d9f9b987c1553/invoke.js"></script>
         <AdsF1 />
         <AdsF1 />
         <AdsF1 />
