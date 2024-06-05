78f01ca9ce1c29e21d79d1f5a4626039b93d3c62
santokhan <santokhan1999@gmail.com>
2024-06-05 16:02:01 +0600

    set cookie

--- home.php
+++ home.php
@@ -1,6 +1,10 @@
 <?php // http://localhost:8001?hash=abc123 ?>
 
 <?php 
+session_start();
+?>
+
+<?php 
 include './src/utils/get_original_url.php';
 ?>
 
@@ -10,6 +14,12 @@ $hash = isset($_GET['hash']) ? $_GET['hash'] : '';
 $_SESSION['temp_hash'] = $hash;
 // Use the hash as needed
 // unset($_SESSION['temp_hash']); // Destroy the hash after use
+?>
+
+<?php
+setcookie('temp_hash', $hash, time() + (86400 * 1), '/'); // Cookie expires in 1 day
+// Use the hash as needed
+// setcookie('temp_hash', '', time() - 3600, '/'); // Destroy the cookie after use
 ?>
 
 <div class="mx-auto w-full max-w-screen-xl p-4">
--- src/continue-button.php
+++ src/continue-button.php
@@ -1,3 +1,10 @@
-<a href="#" id="scrollToBottom" class="inline-flex items-center justify-center rounded-lg bg-blue-700 px-4 py-2.5 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 flex gap-2 items-center">Continue<svg class="size-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+<?php
+$hash = '';
+if (isset($_COOKIE["temp_hash"])) {
+    $hash = $_COOKIE["temp_hash"];
+}
+?>
+
+<a href="/" title="<?php echo $hash; ?>" id="scrollToBottom" class="inline-flex items-center justify-center rounded-lg bg-blue-700 px-4 py-2.5 text-center text-base font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 flex gap-2 items-center">Continue<svg class="size-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
     </svg></a>
