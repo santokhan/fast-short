6151bec1f4d1c9eeab840cd8723da146ae5f0c6b
santokhan <santokhan1999@gmail.com>
2024-06-07 19:16:43 +0600

    again add cors to multiple place

--- app/api/get-url/route.js
+++ app/api/get-url/route.js
@@ -11,15 +11,16 @@ export async function GET(request) {
     if (urlObject) {
         const { URL } = urlObject
         const res = { url: URL }; // {res: { url: 'https://example.com' }}
-        // return new Response(JSON.stringify(res), {
-        //     headers: {
-        //         'Access-Control-Allow-Origin': '*',
-        //         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        //         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-        //         'Content-Type': 'application/json' // Set Content-Type header for JSON response
-        //     }
-        // });
-        return Response.json(res)
+
+        return new Response(JSON.stringify(res), {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+                'Content-Type': 'application/json' // Set Content-Type header for JSON response
+            }
+        });
+        // return Response.json(res)
     } else {
         return Response.json({
             error: 'URL not found',
