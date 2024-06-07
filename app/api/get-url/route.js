e36e9d7eb25b182162fe329a634f30a53b24edf1
santokhan <santokhan1999@gmail.com>
2024-06-07 19:00:53 +0600

    add cors to nextjs route for php app

--- app/api/get-url/route.js
+++ app/api/get-url/route.js
@@ -1,15 +1,24 @@
+export const dynamic = 'force-dynamic' // defaults to auto
 import getURL from "@/actions/query/getURL"
 
 // Building API: http://localhost:3000/api/get-url?hash=2Yze1mFA
-
 export async function GET(request) {
     const { searchParams } = new URL(request.url)
     const hash = searchParams.get('hash')
 
-    const url = await getURL(hash)
+    const urlObject = await getURL(hash)
 
-    if (url) {
-        return Response.json(url)
+    if (urlObject) {
+        const { URL } = urlObject
+        const res = JSON.stringify({ url: URL }); // {res: { url: 'https://example.com' }}
+        return new Response(res, {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+                'Content-Type': 'application/json' // Set Content-Type header for JSON response
+            }
+        });
     } else {
         return Response.json({
             error: 'URL not found',
