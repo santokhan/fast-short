238ed4750f1506360b9164dc85b78ad98aaaa740
santokhan <santokhan1999@gmail.com>
2024-06-10 04:57:04 +0600

    hotfix

--- components/RedirectButton.jsx
+++ components/RedirectButton.jsx
@@ -2,16 +2,22 @@ import Link from 'next/link'
 import { ArrowUp } from 'iconsax-react'
 
 const RedirectButton = ({ redirectURL = '' }) => {
-    return (
-        <Link
-            href={redirectURL}
-            className='flex justify-center items-center gap-1 px-4 py-3 rounded-lg bg-button font-medium text-white'
-            title='Open Link'
-            target='_blank'
-        >
-            <ArrowUp className='size-6 rotate-45' />
-        </Link>
-    )
+    if (redirectURL) {
+        if (!redirectURL.startsWith('http')) {
+            redirectURL = 'https://' + redirectURL
+        }
+
+        return (
+            <Link
+                href={redirectURL}
+                className='flex justify-center items-center gap-1 px-4 py-3 rounded-lg bg-button font-medium text-white'
+                title='Open Link'
+                target='_blank'
+            >
+                <ArrowUp className='size-6 rotate-45' />
+            </Link>
+        )
+    }
 }
 
 export default RedirectButton
