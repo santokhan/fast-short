2cf09e3ffd8526df418bae65dada3fe3a9e61f99
santokhan <santokhan1999@gmail.com>
2024-06-11 00:52:13 +0600

    update ads scripts

--- src/adsense-ads/top-ads.php
+++ src/adsense-ads/top-ads.php
@@ -1,1 +1,4 @@
 <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3279946764061711" data-ad-slot="2150066632" data-ad-format="auto" data-full-width-responsive="true"></ins>
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
