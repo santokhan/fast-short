3edb8c06b5b6fca6f7efd84313c71ac818ca977f
santokhan <santokhan1999@gmail.com>
2024-06-11 00:59:31 +0600

    update ads name

--- src/adsense-ads/multiple-ads.php
+++ src/adsense-ads/multiple-ads.php
@@ -1,4 +1,4 @@
-<ins class="bottomadsmultiple" style="display:block" data-ad-format="autorelaxed" data-ad-client="ca-pub-3279946764061711" data-ad-slot="2500183321"></ins>
+<ins class="adsbygoogle" style="display:block" data-ad-format="autorelaxed" data-ad-client="ca-pub-3279946764061711" data-ad-slot="2500183321"></ins>
 <script>
     (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
