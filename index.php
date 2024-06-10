ebafd8ac7b14aedbfdc49a0368788dc920a4910b
santokhan <santokhan1999@gmail.com>
2024-06-11 00:23:08 +0600

    update ads size

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ ini_set('display_errors', 1);
             <div class="">
                 <?php // include './src/adsterra-ads/ads_popunder.php'; ?>
             </div>
-            <div class="">
+            <div class="w-full">
                 <?php include './src/adsense-ads/multiple-ads.php'; ?>
             </div>
         </div>
