401801d4e9a7fd3a1bd93f42a6054c74dec4aee6
santokhan <santokhan1999@gmail.com>
2024-06-11 14:54:06 +0600

    update

--- generating-link.php
+++ generating-link.php
@@ -6,7 +6,7 @@ include './src/components/continue-button.php';
     <?php include './includes/job_cards.php'; ?>
     <?php include './src/countdown.php'; ?>
 
-    <div class="flex justify-center itmes-center">
+    <div class="flex justify-center itmes-center" id="scrollToBottom">
         <?php
         if (function_exists('anchor')) {
             anchor('/get-link', 'continueHidden');
--- get_link.php
+++ get_link.php
@@ -1,26 +1,9 @@
-<?php // http://localhost:8001?hash=abc123 
-?>
-
-<?php
-$hash = isset($_GET['hash']) ? $_GET['hash'] : '';
-
-$_SESSION['temp_hash'] = $hash;
-// Use the hash as needed
-// unset($_SESSION['temp_hash']); // Destroy the hash after use
-?>
-
-<?php
-setcookie('temp_hash', $hash, time() + (86400 * 1), '/'); // Cookie expires in 1 day
-// Use the hash as needed
-// setcookie('temp_hash', '', time() - 3600, '/'); // Destroy the cookie after use
-?>
-
 <div class="mx-auto w-full max-w-screen-xl p-4">
     <h1 class="text-3xl font-bold text-center hidden">Shorten a long URL</h1>
 
     <?php include 'includes/job_cards.php'; ?>
 
-    <div class="flex justify-center itmes-center">
+    <div class="flex justify-center itmes-center" id="scrollToBottom">
         <?php include './src/get_link_button.php'; ?>
     </div>
     <div class="w-full">
