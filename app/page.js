dd8934366a6116bc2255319ef6083767671a02fd
santokhan <santokhan1999@gmail.com>
2024-06-14 20:11:46 +0600

    remove generated page and update form

--- app/page.js
+++ app/page.js
@@ -1,51 +1,26 @@
 'ues server'
 
-import { AdsF1 } from '@/components/AdsScript'
 import NavbarSpace from '@/components/NavbarSpace'
 import Section from '@/components/Section'
 import Footer from '@/components/footer/Footer'
 import URLShort from '@/components/form/URLShorter'
 import NavBar from '@/components/navbar/NavBar'
 import AuthProvider from '@/context/AuthContext'
-import Script from 'next/script'
 
 export default async function URLShortFormPage({ searchParams }) {
-  // const error = { ...searchParams }
-
   return (
     <>
       <NavBar />
       <main className='min-h-screen'>
         <NavbarSpace />
         <Section className='space-y-8'>
-          {/* <URLShorterForm error={error} /> */}
           <AuthProvider>
             <URLShort />
           </AuthProvider>
         </Section>
-        {/* Ads */}
         <div className='flex justify-center items-center'>
           <div className="mt-10 ad-script-setup"></div>
         </div>
-        {/* Ads End */}
-        {/* <Script
-          id="ad-script-setup"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              atOptions = {
-                'key' : 'd592f8f84f38de0159699b8ddb6fc839',
-                'format' : 'iframe',
-                'height' : 250,
-                'width' : 300,
-                'params' : {}
-              };
-            `,
-          }}
-        /> */}
-        {/* <AdsF1 /> */}
-        {/* <AdsF1 /> */}
-        {/* <AdsF1 /> */}
       </main>
       <Footer />
     </>
--- components/form/URLShorter.jsx
+++ components/form/URLShorter.jsx
@@ -5,27 +5,54 @@ import createURL from '@/actions/query/createURL'
 import shortenUrlToId from '@/lib/shorter/shortid'
 import React, { useState } from 'react'
 import placeholder from './shorter-form/placeholder'
-import { useRouter } from 'next/navigation'
 import { storeToLocal } from '@/lib/local-storage/localStorage'
 import SubmitButton from './shorter-form/SubmitButton'
 import { twMerge } from 'tailwind-merge'
 import DomainSelect from './shorter-form/SelectDomain'
 import FormTitle from '@/components/form/FormTitle'
 import { useSession } from 'next-auth/react'
+import CopyButton from '../button/CopyButton'
+import RedirectButton from '../RedirectButton'
+import { adsURL } from './shorter-form/domains'
+
+const ShortenURL = ({ res = '' }) => {
+    if (res) {
+        const URLWithHash = adsURL[0] + '?hash=' + (res.alias || res.hash)
+
+        return (
+            <div className='mt-8 flex flex-col items-center border rounded-xl p-4 lg:p-8'>
+                <h2 className='text-2xl text-slate-700'>Here is the shorten URL</h2>
+
+                <div className='flex items-center flex-wrap justify-center gap-2 mt-6'>
+                    <div className='px-4 py-3 bg-gray-50 flex items-center rounded-lg w-full max-w-lg'>
+                        <p className='w-full overflow-x-hidden text-ellipsis whitespace-nowrap'>{URLWithHash}</p>
+                    </div>
+                    <CopyButton url={URLWithHash} />
+                    <RedirectButton redirectURL={URLWithHash} />
+                </div>
+            </div>
+        )
+    }
+}
 
 const UrlShorterForm = () => {
+    const [generated, setGenerated] = useState('')
     const [url, setUrl] = useState('')
     const [domain, setDomain] = useState('fastshort.xyz')
     const [alias, setAlias] = useState('')
     const [error, setError] = useState({})
     const [loading, setLoading] = useState(false)
-    const router = useRouter()
     const { data } = useSession()
+    const inputStyle = `block px-4 py-4 w-full rounded-lg bg-gray-50`
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
         const author = data?.user?.email || ''
+
+        if (!url.startsWith('https://') || !url.startsWith('http://')) {
+            setUrl((url) => 'https://' + url)
+        }
 
         if (url && domain) {
             if (alias) {
@@ -50,7 +77,7 @@ const UrlShorterForm = () => {
 
                             storeToLocal(res)
 
-                            router.push(`/generated/${alias}`)
+                            setGenerated(res)
                         }
                     }
                 } catch (error) {
@@ -80,7 +107,7 @@ const UrlShorterForm = () => {
 
                         storeToLocal(res)
 
-                        router.push(`/generated/${shortenedId}`)
+                        setGenerated(res)
                     }
                 } catch (err) {
                     console.error(err)
@@ -90,61 +117,55 @@ const UrlShorterForm = () => {
         }
     }
 
-    function setFormValue(key, value = '') {
-        if (key) {
-            //
-            setError({})
-        }
-    }
-
-    const inputStyle = `block px-4 py-4 w-full rounded-lg bg-gray-50`
-
     return (
-        <div className='p-6 lg:p-8 rounded-xl bg-white space-y-6 mx-auto max-w-2xl'>
-            <FormTitle>Shorten a long URL</FormTitle>
-            <form onSubmit={handleSubmit} className='flex flex-wrap gap-6'>
-                <label className='w-full flex flex-col items-start'>
-                    <input
-                        type='text'
-                        name='url'
-                        value={url}
-                        onChange={(e) => {
-                            setUrl(e.target.value)
-                            setError({})
-                        }}
-                        className={twMerge(inputStyle)}
-                        placeholder={placeholder.url}
-                        required
-                        pattern='^[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$'
-                    />
-                </label>
-                <label className='flex-1'>
-                    <DomainSelect
-                        value={domain}
-                        onChange={(value) => {
-                            setDomain(value)
-                            setError({})
-                        }}
-                        className={twMerge(inputStyle)}
-                        required={true}
-                    />
-                </label>
-                <label className='flex-1'>
-                    <input
-                        type='text'
-                        name='alias'
-                        value={alias}
-                        onChange={(e) => {
-                            setAlias(e.target.value)
-                            setError({})
-                        }}
-                        className={twMerge(inputStyle)}
-                        placeholder={placeholder.alias}
-                    />
-                    {error.alias && <p className='text-red-500'>{error.alias}</p>}
-                </label>
-                <SubmitButton loading={loading} />
-            </form>
+        <div className='mx-auto max-w-2xl'>
+            <div className='p-4 lg:p-8 rounded-xl bg-white space-y-6 border'>
+                <FormTitle>Shorten a long URL</FormTitle>
+                <form onSubmit={handleSubmit} className='flex flex-wrap gap-6'>
+                    <label className='w-full flex flex-col items-start'>
+                        <input
+                            type='text'
+                            name='url'
+                            value={url}
+                            onChange={(e) => {
+                                setUrl(e.target.value)
+                                setError({})
+                            }}
+                            className={twMerge(inputStyle)}
+                            placeholder={placeholder.url}
+                            required
+                            pattern='^[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$'
+                        />
+                    </label>
+                    <label className='flex-1'>
+                        <DomainSelect
+                            value={domain}
+                            onChange={(value) => {
+                                setDomain(value)
+                                setError({})
+                            }}
+                            className={twMerge(inputStyle)}
+                            required={true}
+                        />
+                    </label>
+                    <label className='flex-1'>
+                        <input
+                            type='text'
+                            name='alias'
+                            value={alias}
+                            onChange={(e) => {
+                                setAlias(e.target.value)
+                                setError({})
+                            }}
+                            className={twMerge(inputStyle)}
+                            placeholder={placeholder.alias}
+                        />
+                        {error.alias && <p className='text-red-500'>{error.alias}</p>}
+                    </label>
+                    <SubmitButton loading={loading} />
+                </form>
+            </div>
+            <ShortenURL res={generated} />
         </div>
     )
 }
