d51879d8d96b33e5619d3d57743afbda845f3a5a
santokhan <santokhan1999@gmail.com>
2024-06-19 15:32:12 +0600

    add canonical

--- includes/head.php
+++ includes/head.php
@@ -1,4 +1,5 @@
 <meta charset="UTF-8">
+<link rel="canonical" href="https://fastshort.xyz">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="16x16">
