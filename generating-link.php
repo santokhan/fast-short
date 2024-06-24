0c7c1ff946108489d5e5bc4fbdbc36a524aa5745
santokhan <santokhan1999@gmail.com>
2024-06-25 02:03:43 +0600

    update margin

--- generating-link.php
+++ generating-link.php
@@ -3,8 +3,12 @@ include './src/components/continue-button.php';
 ?>
 
 <div class="mx-auto w-full max-w-screen-xl p-4">
-    <?php include './includes/job_cards.php'; ?>
-    <?php include './src/countdown.php'; ?>
+    <div class="">
+        <?php include './includes/job_cards.php'; ?>
+    </div>
+    <div class="py-4 mt-4">
+        <?php include './src/countdown.php'; ?>
+    </div>
 
     <div class="flex justify-center itmes-center" id="scrollToBottom">
         <?php
