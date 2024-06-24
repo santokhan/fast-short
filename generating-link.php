8be7abb74c10e3f64d8a19b4aa5de82f5b86d783
santokhan <santokhan1999@gmail.com>
2024-06-25 02:24:52 +0600

    update captcha style

--- generating-link.php
+++ generating-link.php
@@ -6,15 +6,15 @@ include './src/components/continue-button.php';
     <div class="">
         <?php include './includes/job_cards.php'; ?>
     </div>
-    <div class="py-4 mt-4">
+    <div class="py-8">
         <?php include './src/countdown.php'; ?>
-    </div>
-
-    <div class="flex justify-center itmes-center" id="scrollToBottom">
-        <?php
-        if (function_exists('anchor')) {
-            anchor('/get-link', 'continueHidden');
-        }
-        ?>
+        
+        <div class="flex justify-center itmes-center" id="scrollToBottom">
+            <?php
+            if (function_exists('anchor')) {
+                anchor('/get-link', 'continueHidden');
+            }
+            ?>
+        </div>
     </div>
 </div>
--- get_link.php
+++ get_link.php
@@ -1,11 +1,12 @@
 <div class="mx-auto w-full max-w-screen-xl p-4">
     <h1 class="text-3xl font-bold text-center hidden">Shorten a long URL</h1>
-    <?php
-    include 'includes/job_cards.php';
-    include './src/recaptcha/recaptcha.php';
-    ?>
-    <div class="flex justify-center itmes-center mt-4" id="scrollToBottom">
-        <?php include './src/get_link_button.php'; ?>
+    <?php include 'includes/job_cards.php'; ?>
+    <div class="py-8">
+        <?php include './src/recaptcha/recaptcha.php'; ?>
+
+        <div class="flex justify-center itmes-center mt-4" id="scrollToBottom">
+            <?php include './src/get_link_button.php'; ?>
+        </div>
     </div>
     <div class="w-full">
         <?php include './src/adsense-ads/multiple-ads.php'; ?>
