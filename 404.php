ed2a334e5bf0f0812b6948d730a0f44b8e139919
santokhan <santokhan1999@gmail.com>
2024-06-29 14:04:48 +0600

    add 404

---
+++ 404.php
@@ -0,0 +1,8 @@
+<div class="bg-gray-100 flex items-center justify-center h-screen">
+    <div class="text-center">
+        <h1 class="text-6xl font-bold text-gray-800">404</h1>
+        <p class="text-2xl text-gray-600 mt-4">Page Not Found</p>
+        <p class="text-gray-500 mt-2">The page you are looking for does not exist.</p>
+        <a href="/" class="text-indigo-600 hover:underline mt-4 inline-block">Go back home</a>
+    </div>
+</div>
