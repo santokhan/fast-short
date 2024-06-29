c06a603256596df2b8a6088cc7568e97c2020ea9
santokhan <santokhan1999@gmail.com>
2024-06-29 16:54:10 +0600

    update footer

--- includes/footer.php
+++ includes/footer.php
@@ -3,23 +3,42 @@ include './src/utils/navs.php';
 ?>
 
 <footer class="bg-white">
-    <div class="container mx-auto p-4 sm:px-6">
-        <div class="w-full md:flex md:items-center md:justify-between gap-4">
-            <div class="flex items-center py-2">
-                <?php include './src/logo.php'; ?>
+    <div class="container mx-auto px-4 sm:px-6 py-12">
+        <div class="w-full flex flex-wrap md:items-start gap-8">
+            <div class="basis-80 flex-grow flex-shrink-0">
+                <div class=""><?php include './src/logo.php'; ?></div>
+                <div class="mt-8">
+                    <?php include './src/components/social.php'; ?>
+                </div>
             </div>
 
-            <ul class="flex flex-wrap gap-x-4 sm:gap-x-6 items-center">
-                <?php foreach ($menuItems as $item) : ?>
-                    <li><a href="<?php echo htmlspecialchars($item['url']); ?>" class="hover:underline"><?php echo htmlspecialchars($item['label']); ?></a></li>
-                <?php endforeach; ?>
+            <ul class="basis-80 flex-grow flex-shrink-0">
+                <h5 class="text-lg mb-4 font-bold">Resources</h5>
+                <div class="flex flex-col gap-2 items-start">
+                    <?php foreach ($menuItems as $item) : ?>
+                        <a href="<?php echo htmlspecialchars($item['url']); ?>" class="block hover:underline"><?php echo htmlspecialchars($item['label']); ?></a>
+                    <?php endforeach; ?>
+                </div>
+            </ul>
+            <ul class="basis-80 flex-grow flex-shrink-0">
+                <h5 class="text-lg mb-4 font-bold">Others</h5>
+                <div class="flex flex-col gap-2 items-start">
+                    <?php foreach ($resources_navs as $item) : ?>
+                        <a href="<?php echo htmlspecialchars($item['url']); ?>" class="block hover:underline"><?php echo htmlspecialchars($item['label']); ?></a>
+                    <?php endforeach; ?>
+                </div>
+            </ul>
+            <ul class="basis-80 flex-grow flex-shrink-0">
+                <h5 class="text-lg mb-4 font-bold">Guideline</h5>
+                <div class="flex flex-col gap-2 items-start">
+                    <?php foreach ($guide as $item) : ?>
+                        <a href="<?php echo htmlspecialchars($item['url']); ?>" class="block hover:underline"><?php echo htmlspecialchars($item['label']); ?></a>
+                    <?php endforeach; ?>
+                </div>
             </ul>
         </div>
     </div>
     <div class="container mx-auto p-4 sm:px-6">
-        <div class="w-full md:flex md:items-center md:justify-between gap-4">
-            <p class="sm:text-center">© 2023 Santo. All Rights Reserved.</p>
-            <?php include './src/components/social.php'; ?>
-        </div>
+        <p class="text-center">© 2023 Santo. All Rights Reserved.</p>
     </div>
 </footer>
--- src/utils/navs.php
+++ src/utils/navs.php
@@ -5,3 +5,15 @@ $menuItems = [
     ['url' => '/privacy-policy', 'label' => 'Privacy Policy'],
     ['url' => '/terms', 'label' => 'Terms & Conditions']
 ];
+
+$resources_navs = [
+    ['url' => '/faq', 'label' => 'FAQ'],
+    ['url' => '/gallery', 'label' => 'Gallery'],
+    ['url' => '/sitemap', 'label' => 'Sitemap'],
+    ['url' => '/events', 'label' => 'Events Calender'],
+];
+
+$guide = [
+    ['url' => '/affiliate', 'label' => 'Affiliate'],
+    ['url' => '/guide', 'label' => 'Guide'],
+];
