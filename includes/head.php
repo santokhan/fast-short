669a16a89a82c49037e7c265623b732a0d190ade
santokhan <santokhan1999@gmail.com>
2024-06-29 21:19:51 +0600

    add analytics

--- includes/head.php
+++ includes/head.php
@@ -20,6 +20,16 @@
 <link rel="stylesheet" href="./assets/css/style.css">
 
 <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-PMXVDMG16R"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-PMXVDMG16R');
+</script>
+
+<!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-HQ4YE72CMH"></script>
 <script>
      window.dataLayer = window.dataLayer || [];
