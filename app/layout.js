30bd5f98642012dd66f0fa68948fe8322711565e
santokhan <santokhan1999@gmail.com>
2024-07-06 10:32:23 +0600

    add adsense script

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google';
-import Script from 'next/script';
 import './globals.css';
+import AdsenseScript from '@/components/AdsenseScript';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,10 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* <Script
-          src="//www.topcreativeformat.com/d592f8f84f38de0159699b8ddb6fc839/invoke.js"
-          strategy="afterInteractive"
-        /> */}
+        <AdsenseScript />
       </head>
       <body className={inter.className}>
         {children}
