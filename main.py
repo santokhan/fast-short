33b64a18ba8b7da36528a34ce8efef19b01d569a
santokhan <santokhan1999@gmail.com>
2024-08-05 03:54:42 +0600

    add cors

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 from app.routers import url, withdrawal, wallet
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(url.router)
 app.include_router(wallet.router)
 app.include_router(withdrawal.router)
